a578a6d64898f6b4b9b6af0839c638bbee582620
91matfri <mattiasfrigren@hotmail.com>
2020-06-22 15:50:10 +0200

    last commit and push before 16:00

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Alldrinks from '../data/AllDrinks.json';
 import Drink from '../components/drink';
 
@@ -8,7 +8,13 @@ const Explore = (props) =>{
 
     const [searchResualt, setSearchResualt] = useState('');
     const [displayDrinks, setDrinkstodisplay] =useState([]);
-    console.log(props.match.params.idName)
+    const [currentView, setView] =useState(props.match.params.idName);
+    const [currentDrink, setDrink] =useState([]);
+
+    useEffect(()=>{
+        setView(props.match.params.idName);
+        console.log(currentView)
+    })
 
     const Search = ()=>{
         let newArrayList = (allCocktails.filter((e)=>{
@@ -29,14 +35,47 @@ const Explore = (props) =>{
         })]);}
         else{setDrinkstodisplay(<p>no result</p>)}
     }
+/*
+försökte få denna functionen att fungera för att lösa subsidor, tyvärr blev tiden för mig lite knapp.
 
+
+    const currentDrinkView =() =>{
+      let drinkside = allCocktails.filter((e)=>{
+          if(e.name.match({currentView})){
+              return e;
+          }
+      })
+      console.log(drinkside.name)
+      console.log(drinkside.image)
+      console.log(drinkside.preparation)
+      
+        setDrink([
+            <div>
+            <h3> {drinkside.name}</h3>
+            <p>{drinkside.preparation}</p>
+            <img src={drinkside.image}></img>
+            </div>
+
+        ])
+    }
+*/
     return(
         <div>
+        {currentView ==='1' ?
+            <div>
         <h1> Find your favorit drink</h1>
         <input id='searchField' onChange = {event =>{setSearchResualt(event.target.value)}}>
         </input>
         <button onClick={Search}> Search</button>
          {displayDrinks}
+         </div> :
+         <div> 
+         <h3>{currentView}</h3>
+         <p></p>
+         </div>
+
+        }
+
         </div>
     )
 }
